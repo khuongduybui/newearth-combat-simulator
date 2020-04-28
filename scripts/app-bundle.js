c286db963e8ebb15da70b9b939649d520f938030
Duy K. Bui <duy@buifamily.info>
2020-04-28 13:07:42 -0400

    build: Update gh pages

--- scripts/app-bundle.js
+++ scripts/app-bundle.js
@@ -14,8 +14,8 @@ define('app',["exports", "numeral"], function (_exports, _numeral) {
       this.battleLogs = [];
       this.buffs = {
         attack: 0.75,
-        defense: 1.5066,
-        health: 0.51,
+        defense: 1.5466,
+        health: 0.52,
         attackItem: 0,
         defenseItem: 0,
         combatShip: 0.2
@@ -24,7 +24,7 @@ define('app',["exports", "numeral"], function (_exports, _numeral) {
         name: 'hero',
         qty: 1,
         tier: 0,
-        attack: 9500,
+        attack: 19500,
         defense: 100,
         health: 245,
         attackBuff: 0.56,
