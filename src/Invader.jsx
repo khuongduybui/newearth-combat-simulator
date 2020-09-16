598b86f49ad7dd931e78e3f6d71fe1dd505de28c
Duy K. Bui <duy@buifamily.info>
2020-09-15 22:06:18 -0400

    refactor: Use effects

--- src/Invader.jsx
+++ src/Invader.jsx
@@ -1,5 +1,5 @@
 import * as numeral from 'numeral';
-import { createState, createEffect } from 'solid-js';
+import { createState, createEffect, createDependentEffect } from 'solid-js';
 
 const Invader = () => {
   const [state, setState] = createState({
@@ -10,25 +10,36 @@ const Invader = () => {
     bugLevel: 1,
     bugHealth: 0,
     chain: 1,
+    damage: 0,
+    energy: 0,
   });
   if (localStorage.getItem('invader')) setState(JSON.parse(localStorage.getItem('invader')));
   createEffect(() => localStorage.setItem('invader', JSON.stringify(state)));
 
-  const bindToState = (field, additional = () => ({})) => (e) => setState({ [field]: e.target.value, ...additional(e.target.value) });
+  const bindToState = (field) => (e) => setState({ [field]: e.target.value });
 
   const num = (field) => numeral(state[field]).value();
   const numFormat = (field) => numeral(state[field]).format('0,0');
-  // const percentFormat = (field) => numeral(state[field]).format('0%');
 
   const damage = (chain) => num('heroAttack') * (1 + num('invaderAttack') + num('combatShip')) * (chain * (1 + (chain - 1) * 0.025));
-  const computeChain = (bugHealthInput) => {
-    const bugHealth = numeral(bugHealthInput).value();
-    let currentChain = 1,
-      currentDamage = damage(currentChain);
-    while (currentDamage < bugHealth) currentDamage = damage(++currentChain);
-    return { chain: currentChain };
-  };
-  const energy = () => (num('bugLevel') + 1) * 100 * (1 - num('energyUsage')) * num('chain');
+
+  createDependentEffect(
+    () => {
+      let chain = 1,
+        currentDamage = damage(chain);
+      while (currentDamage < num('bugHealth') && chain < 41) currentDamage = damage(++chain);
+      setState({ chain });
+    },
+    [() => num('heroAttack'), () => num('invaderAttack'), () => num('combatShip'), () => num('bugHealth')],
+    true,
+  );
+
+  createEffect(() => {
+    setState({
+      damage: damage(num('chain')),
+      energy: (num('bugLevel') + 1) * 100 * (1 - num('energyUsage')) * num('chain'),
+    });
+  });
 
   return (
     <>
@@ -36,20 +47,16 @@ const Invader = () => {
       <p>Please input your hero attack damage, invader attack buff, and combat ship buff if any.</p>
       <h2>Configure</h2>
       <label>
-        Hero Attack
-        <input type='text' value={numFormat('heroAttack')} onInput={bindToState('heroAttack')}></input>
+        Hero Attack (e.g. <code>100,000</code>)<input type='text' value={numFormat('heroAttack')} onInput={bindToState('heroAttack')}></input>
       </label>
       <label>
-        Invader Attack
-        <input type='text' value={state.invaderAttack} onInput={bindToState('invaderAttack')}></input>
+        Hero Invader Attack (e.g. <code>70%</code>)<input type='text' value={state.invaderAttack} onInput={bindToState('invaderAttack')}></input>
       </label>
       <label>
-        Energy Usage
-        <input type='text' value={state.energyUsage} onInput={bindToState('energyUsage')}></input>
+        Invader Energy Cost Reduction (e.g. <code>35%</code>)<input type='text' value={state.energyUsage} onInput={bindToState('energyUsage')}></input>
       </label>
       <label>
-        Combat Ship
-        <input type='text' value={state.combatShip} onInput={bindToState('combatShip')}></input>
+        Combat Ship (e.g. <code>25%</code>)<input type='text' value={state.combatShip} onInput={bindToState('combatShip')}></input>
       </label>
 
       <p>Now you can input Bug Health and the tool will suggest the required chain to kill it.</p>
@@ -60,22 +67,22 @@ const Invader = () => {
       </label>
       <label>
         Bug Health
-        <input type='text' value={numFormat('bugHealth')} onInput={bindToState('bugHealth', computeChain)}></input>
+        <input type='text' value={numFormat('bugHealth')} onInput={bindToState('bugHealth')}></input>
       </label>
 
       <p>Alternatively, feel free to adjust the chain to see the expected damage.</p>
       <h2>Chain</h2>
       <label>
         Chain: {state.chain}
-        <input type='range' min='1' max='40' value={state.chain} onInput={bindToState('chain')}></input>
+        <input type='range' min='1' max='41' value={state.chain} onInput={bindToState('chain')}></input>
       </label>
       <label>
         Damage
-        <input type='text' readonly value={numeral(damage(num('chain'))).format('0,0')}></input>
+        <input type='text' readonly value={numFormat('damage')}></input>
       </label>
       <label>
         Energy
-        <input type='text' readonly value={numeral(energy()).format('0,0')}></input>
+        <input type='text' readonly value={numFormat('energy')}></input>
       </label>
     </>
   );
